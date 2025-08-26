9158380b4c5029008c7b70ce44d7779caafe70fe
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-08-25 22:18:36 -0400

    feat: task

---
+++ tasks/src/task/task.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { GrpcMethod } from '@nestjs/microservices';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @GrpcMethod('TaskService', 'CreateTask')
+  async createTask(data: CreateTaskDto) {
+    const t = await this.taskService.create(data);
+    return { task: this.map(t) };
+  }
+
+  @GrpcMethod('TaskService', 'FindAllTasks')
+  async findAllTasks(_: {}) {
+    const list = await this.taskService.findAll();
+    return { tasks: list.map(this.map) };
+  }
+
+  private map = (t: any) => ({
+    id: t.id,
+    title: t.title,
+    description: t.description,
+    completed: t.completed,
+    created_by: t.createdBy,
+    created_at: t.createdAt?.toISOString?.() ?? String(t.created_at),
+  });
+}
---
+++ tasks/src/task/task.service.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RpcException, type ClientGrpc } from '@nestjs/microservices';
+import { UserServiceClient } from 'src/proto/user';
+import { lastValueFrom } from 'rxjs';
+import { status } from '@grpc/grpc-js';
+
+@Injectable()
+export class TaskService {
+
+  private userService!: UserServiceClient;
+
+  constructor(
+    @InjectRepository(Task) 
+    private readonly taskRepository: Repository<Task>,
+    @Inject('USER_PACKAGE') private client: ClientGrpc,
+  ) {}
+
+  onModuleInit() {
+    this.userService = this.client.getService<UserServiceClient>('UserService');
+  }
+
+  async create(createTaskDto: CreateTaskDto) {
+    const task = this.taskRepository.create(createTaskDto);
+
+    try {
+      await lastValueFrom(this.userService.findOne({ id: task.createdBy }));
+    } catch (e) {
+      throw new RpcException({ code: status.NOT_FOUND, message: 'User does not exist' });
+    }
+
+    return this.taskRepository.save(task);
+  }
+
+  findAll() {
+    return this.taskRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} task`;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return `This action updates a #${id} task`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} task`;
+  }
+}
